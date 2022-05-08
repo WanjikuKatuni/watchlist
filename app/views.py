f501dcc2ef58827df8b441533fc0c503bfac9037
wanjiku <annritawanjiku77@gmail.com>
2022-05-08 05:53:41 +0300

    add variable

---
+++ app/views.py
@@ -0,0 +1,14 @@
+from flask import render_template #takes name of the template file index.html as first argument and searchs it in the app/template subdirectory
+from app import app #impot app instance from the app folder
+
+
+#views
+@app.route('/')
+def index():
+
+    ''' 
+    view root page function which returns the index page and the data it has 
+    '''
+    message="Hello WAnjiku!!!!!!"
+    
+    return render_template('index.html', message = message)  #first messgae is the variable in the  template, the second message is variablein the view function
