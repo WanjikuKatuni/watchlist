2a7443933d4bc5ff16b09b288dd7dedf324f1d44
wanjiku <annritawanjiku77@gmail.com>
2022-05-08 06:59:35 +0300

    add configuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask 
+from .config import DevConfig
 
 #init app
 app= Flask(__name__)
+
+#set up config
+app.config.from_object(DevConfig) #Setup configuration and pass the devconfig subclass
 
 from app import views #this allows us to create views
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 from app import app #import app instance
 
 if __name__ == '__main__':
-    app.run(debug=True)    #check if script is urn directly using the app.run() meethod to launch server byt in debug mode
+    app.run()    
