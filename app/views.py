ffb0e65784b8dc95077136533f729cd8c8fa1b01
wanjiku <annritawanjiku77@gmail.com>
2022-05-08 11:53:49 +0300

    add api key

--- app/views.py
+++ app/views.py
@@ -9,10 +9,11 @@ def index():
     ''' 
     view root page function which returns the index page and the data it has 
     '''
-    message="Hello WAnjiku!!!!!!"
+    title='Home - Welcome to The best Movie Review Website Online'
     
-    return render_template('index.html', message = message)  #first messgae is the variable in the  template, the second message is variablein the view function
+    return render_template('index.html', title=title)  #first messgae is the variable in the  template, the second message is variablein the view function
 
 @app.route('/movie/<int:movie_id>')
 def movie(movie_id):
+    title = 'Movie {{ic}}'
     return render_template('movie.html', id=movie_id)
