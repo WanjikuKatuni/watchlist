e1357a611821648b4f71e3fec2e50b5d8b79f568
wanjiku <annritawanjiku77@gmail.com>
2022-05-08 12:14:09 +0300

    connect instance folder confit to app instance

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,10 @@ from flask import Flask
 from .config import DevConfig
 
 #init app
-app= Flask(__name__)
+app= Flask(__name__,instance_relative_config = True) #instance_relative allows us to connect to instance. folder
 
 #set up config
 app.config.from_object(DevConfig) #Setup configuration and pass the devconfig subclass
+app.config.from_pyfile('config.py') #connects to config file in app folder and appened by app.config
 
 from app import views #this allows us to create views
