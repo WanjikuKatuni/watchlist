4002fc10ea3501d5580ccba34dac5e7e4d1505bc
wanjiku <annritawanjiku77@gmail.com>
2022-05-09 19:59:46 +0300

    use bootstrap to add navbar

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask 
 from .config import DevConfig
+from flask_bootstrap import Bootstrap
 
 #init app
 app= Flask(__name__,instance_relative_config = True) #instance_relative allows us to connect to instance. folder
@@ -7,5 +8,10 @@ app= Flask(__name__,instance_relative_config = True) #instance_relative allows u
 #set up config
 app.config.from_object(DevConfig) #Setup configuration and pass the devconfig subclass
 app.config.from_pyfile('config.py') #connects to config file in app folder and appened by app.config
+
+#intializing flask extensions
+bootstrap = Bootstrap(app) #pass in the app instance
+
+
 
 from app import views #this allows us to create views
