41da7e7f7446c1235e2189345f5baec182af2f3f
wanjiku <annritawanjiku77@gmail.com>
2022-05-16 19:22:34 +0300

    create flask shell

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 #from app import app #import app instance
-from app import create_app #import create app function from app folder
+from app import create_app,db #import create app function from app folder, import db  from app.intit file as well to create  python shelll using flaskscript
 from flask_script import Manager, Server #manager innitialise extension and server launches server
-
+from app.models import User #import user class from the models.py
 
 #CREATE APP INSTANCE
 app = create_app('development')
@@ -21,3 +21,11 @@ def test():
 
 if __name__ == '__main__':
     manager.run()    
+
+@manager.shell #used to create the shell context
+def make_shell_context(): #allows to pass properties into the shell
+    return dict(app=app,db=db,User=User) #pass app instance, db instancfe and user class
+    
+
+if __name__=='__main__':
+    manager.run()
